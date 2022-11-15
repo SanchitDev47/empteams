e4e00260dae9b7e4ccce380a17d9b99e62d41e2e
SanchitDev47 <112927220+SanchitDev47@users.noreply.github.com>
2022-11-15 21:41:00 +0530

    delelete icon

--- src/Compoenets/EmpList.tsx
+++ src/Compoenets/EmpList.tsx
@@ -1,19 +1,43 @@
 import * as React from 'react';
 import styled from "styled-components";
+import EditIcon from '@mui/icons-material/Edit';
 import { Avatar, List, ListItemAvatar, ListItem, Typography, Grid, IconButton, ListItemText, Button, Paper, TablePagination, TableContainer, Table, TableBody, TableRow, TableCell, TableHead, Box, Checkbox, FilledInput, FormControl, FormControlLabel, FormLabel, InputAdornment, InputLabel, MenuItem, Radio, RadioGroup, Select, Switch, TextareaAutosize, TextField, SelectChangeEvent } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import MoodIcon from '@mui/icons-material/Mood';
 import ModeIcon from '@mui/icons-material/Mode';
 import { useState } from 'react';
 import { VisibilityOff, Visibility } from '@mui/icons-material';
-import { Form } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
 import { useRequired } from '../common/ts/useRequired';
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
+
+const Transition = React.forwardRef(function Transition(
+    props: TransitionProps & {
+        children: React.ReactElement<any, any>;
+    },
+    ref: React.Ref<unknown>
+) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
 
 export default function EmpList() {
 
     const [visibility, setVisibility] = useState(false);
 
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     function generate(element: React.ReactElement) {
         return [0, 1, 2, 4, 5].map((value) =>
@@ -30,24 +54,34 @@ export default function EmpList() {
             <Header>Employer List</Header>
             <Container>
                 <Grid xs={12}>
-                    <Typography variant="h6" component="div">
-                        List of Employers
-                    </Typography>
                     <List dense={dense}>
                         {generate(
                             <ListItem
                                 secondaryAction={
-                                    <IconButton edge="end" aria-label="delete">
-                                        <DeleteIcon />
-                                    </IconButton>
-
+                                    <>
+                                        <EditIcon />
+                                        <DeleteIcon onClick={handleClickOpen} />
+                                        <Dialog
+                                            open={open}
+                                            TransitionComponent={Transition}
+                                            keepMounted
+                                            onClose={handleClose}
+                                            aria-describedby="alert-dialog-slide-description"
+                                        >
+                                            <DialogTitle>{"Confirmation"}</DialogTitle>
+                                            <DialogContent>
+                                                <DialogContentText id="alert-dialog-slide-description">
+                                                    Sure, You want to delete Employer
+                                                </DialogContentText>
+                                            </DialogContent>
+                                            <DialogActions>
+                                                <Button onClick={handleClose}>NO</Button>
+                                                <Button onClick={handleClose}>Sure</Button>
+                                            </DialogActions>
+                                        </Dialog>
+                                    </>
                                 }
                             >
-                                <ListItemAvatar>
-                                    <Avatar>
-                                        <MoodIcon />
-                                    </Avatar>
-                                </ListItemAvatar>
                                 <ListItemText
                                     primary="#ID"
                                     secondary={secondary ? 'Secondary text' : null}
@@ -72,7 +106,7 @@ export default function EmpList() {
                         )}
                     </List>
                 </Grid>
-            </Container>
+            </Container >
         </>
     )
 }
@@ -88,8 +122,3 @@ gap: 30px;
 const Header = styled.h1`
 text-align: center;
 `;
-// const Singlelist = styled.div`
-// display: flex;
-// background-color:black;
-// gap: 3%;
-// `;
